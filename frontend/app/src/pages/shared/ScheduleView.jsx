28ead456269f255054fb3ea62f446a18063b4b9b
Gonçalo Filipe Brigues Gonçalves <goncalo.goncalves.t0113779@edu.atec.pt>
2026-02-15 19:27:32 +0000

    fix: extend max time in schedule to include the full hour

--- frontend/app/src/pages/shared/ScheduleView.jsx
+++ frontend/app/src/pages/shared/ScheduleView.jsx
@@ -716,7 +716,7 @@ const ScheduleView = () => {
             step={30}
             timeslots={2}
             min={new Date(2020, 0, 1, 7, 0)} // 07:00
-            max={new Date(2020, 0, 1, 23, 0)} // 23:00
+            max={new Date(2020, 0, 1, 23, 59)} // 23:00
             style={{ height: "calc(100% - 60px)" }}
             popup
           />
